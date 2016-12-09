f49451502f2b38a8bf6b1a853d70be8c7897118f
aditya <adityakulkarni@gwmail.gwu.edu>
2016-12-09 18:01:55 -0500

    Created a .py file that can animate the plot and save space on the actual notebook

---
+++ aditya3012/Animate_func.py
@@ -0,0 +1,39 @@
+import numpy
+from matplotlib import pyplot
+import math
+from matplotlib import rcParams
+rcParams['font.family'] = 'serif'
+rcParams['font.size'] =16
+from matplotlib import animation
+from IPython.display import HTML
+
+
+nx =101
+dx =0.1
+nt= 150
+x = numpy.linspace(-10,10,nx)
+D =1
+alpha = 6
+u = 1/2 * numpy.tanh(x) + 1/2
+fig = pyplot.figure(figsize=(8,6))
+ax = pyplot.axes(xlim=(-10,10),ylim=(-0.5,1.5))
+ax.set_xlabel('$x$')
+ax.set_ylabel('$u$')
+#ax.text(0,-0.25,'See how the curve levels off at u =1',color='red')
+ax.annotate('See how the curve levels off at u =1',color='#ff7f0e',xy=(-5,1),xytext=(-9,1.4), arrowprops =dict(facecolor='black', shrink =0.05))
+line = ax.plot([],[],color='c',ls=':',lw=3)[0]
+
+
+def CatchtheFisherwaveanimate(n):
+    dt =0.0001
+    un= numpy.zeros_like(u)
+    for n in range (nt):
+        un= numpy.copy(u)
+        u[1:-1] = un[1:-1] \
+                + D * dt/dx**2 * (un[2:] - 2*un[1:-1] + un[0:-2]) \
+                + alpha * un[1:-1] * dt * (1 - un[1:-1])       
+        u[0] = u[1] 
+        u[-1] = u[-2]
+        line.set_data(x,u)
+anim = animation.FuncAnimation(fig,CatchtheFisherwaveanimate,\
+                               frames=nt,interval=60)
