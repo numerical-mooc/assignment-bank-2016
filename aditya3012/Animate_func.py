189b2acfff812145ca7948d4539ed6cce11626e7
aditya <adityakulkarni@gwmail.gwu.edu>
2016-12-12 14:47:59 -0500

    Thank you Tingyu for solving my git error. New updates to project include added references, intoduction edited and a new animation plot which is visually enticing and took almost a day to deug

--- aditya3012/Animate_func.py
+++ aditya3012/Animate_func.py
@@ -8,26 +8,35 @@ from matplotlib import animation
 from IPython.display import HTML
 
 
-nx =101
-dx =0.1
-nt= 150
+nx =201
+dx =0.2
+nt= 300
 x = numpy.linspace(-10,10,nx)
 D =1
-alpha = 6
-u = 1/2 * numpy.tanh(x) + 1/2
+alpha = 1
+
+u= numpy.zeros(nx)
+lbound = numpy.where(x <= -9)
+ubound = numpy.where(x >= -10)            
+bounds = numpy.intersect1d(lbound, ubound)
+u[bounds]=1       
+
 fig = pyplot.figure(figsize=(8,6))
-ax = pyplot.axes(xlim=(-10,10),ylim=(-0.5,1.5))
+ax = pyplot.axes(xlim=(-10,10),ylim=(0,1.5))
 ax.set_xlabel('$x$')
 ax.set_ylabel('$u$')
 #ax.text(0,-0.25,'See how the curve levels off at u =1',color='red')
-#ax.annotate('See how the curve levels off at u =1',color='#ff7f0e',xy=(-5,1),xytext=(-9,1.4), arrowprops =dict(facecolor='black', shrink =0.05))
+ax.annotate('See how the curve',color='#ff7f0e',wrap= True, xy=(7,1),xytext=(3,1.3))
+ax.annotate('levels off at u =1',color='#ff7f0e',wrap= True,xy=(7,1),xytext=(3,1.2),arrowprops =dict(facecolor='black', shrink =0.04))
 line = ax.plot([],[],color='c',ls=':',lw=3)[0]
 
 
 
 def CatchtheFisherwaveanimate(n):
-    dt =0.0001
+    dt =0.0003
     un= numpy.zeros_like(u)
+    ax.fill_between(x,un,8,interpolate=True, color='white')
+    ax.fill_between(x,u,0, color='green')
     for n in range (nt):
         un= numpy.copy(u)
         u[1:-1] = un[1:-1] \
